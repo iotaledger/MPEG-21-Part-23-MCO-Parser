9d2a8b3c2cef8ea34029efa1f4c3a8f6652e42e7
Pawel Iwan <pawel.ivan@iota.org>
2023-10-17 12:35:47 +0200

    remove unnecessary test and prints

--- test/test-ipfs.js
+++ test/test-ipfs.js
@@ -1,7 +1,6 @@
 const { HeliaOfflineStorage: HeliaOffChainStorage, OffChainStorage: OffChainStorage } = require('../storage/offChainStorage');
 
 const offlineStorage = async () => {
-  console.log("===========NEw storage ===========")
   const offChainStorage = new OffChainStorage();
   await offChainStorage.start();
   const cidUrl = await offChainStorage.publish('a');
@@ -17,8 +16,6 @@ const offlineStorage = async () => {
 
 
 const heliaOfflineStorage = async () => {
-  console.log("============Helia offline stroage ==========")
-
   const offChainStorage = new HeliaOffChainStorage();
   await offChainStorage.start();
   const cidUrl = await offChainStorage.publish('a');
@@ -34,23 +31,6 @@ const heliaOfflineStorage = async () => {
 };
 
 
-
-const heliaIPFS = async () => {
-  const { createHelia } = await import('helia');
-  const { json } = await import('@helia/json');
-  const { CID } = await import('multiformats/cid');
-
-  const helia = await createHelia();
-  await helia.start()
-  const jsonHelia = json(helia);
-
-
-
-  await helia.stop()
-
-
-
-}
 
 offlineStorage().then(() => console.log("done ofline storage"))
 heliaOfflineStorage().then(() => console.log("done helia"))
